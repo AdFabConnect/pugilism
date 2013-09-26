ed87b383c8c24c410fe6ae4f18eedb1068b28210
wonknu <fabrice.labbe@adfab.fr>
2013-09-26 22:59:12 +0200

    server nodeJS and index demo

---
+++ www/config.js
@@ -0,0 +1,11 @@
+/**
+ * CONFIG FILE, to configure different environment [ DEV / PROD ]
+ */
+
+module.exports = {
+    PORT        : 88,
+    DEBUG       : true,
+    IO_LOG      : true,
+    IO_ORIGINS  : '*:*',
+    server      : 'http://192.168.1.108:88/'
+};
