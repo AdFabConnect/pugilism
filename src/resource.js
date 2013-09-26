5655b522ab67e5fd456763cd2622871e822641ff
nicolas labbé <nicolas.labbe@adfab.fr>
2013-09-26 19:16:41 +0200

    pugilism-1 you need to merge now !

--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,10 @@
+var s_playerOne = "res/player-blue.png",
+s_playerTwo = "res/player-purple.png";
+
 var g_resources = [
     //image
-    //{src:s_HelloWorld},
+    {src:s_playerOne},
+    {src:s_playerTwo}
 
     //plist
 
